d97d65412aaabd4a64925d0a2238728d55cc9a62
Kali Lott <kalilott@Kalis-MacBook-Pro.local>
2023-07-17 19:25:49 -0600

    make it better

---
+++ Develop/server.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for homepage
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+// GET Route for retrieving notes
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully!`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+// GET Route for a specific note
+app.get('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    console.info(`${req.method} request received for note ${noteId}`);
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
+  });
+
+// DELETE Route for a specific note
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+)
